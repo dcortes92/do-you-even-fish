184f31ebb3483cbea311fbb1b5eea9fe56c5f2b5
Daniel Cortes <daniel.cortes@rivetlogic.com>
2016-10-12 10:14:05 -0600

    feat(Persising Data): Added base.js
    Persisting data with firebase.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBjvMwj6uLSz_hAW8riFiz87_eyH_j73q8",
+  authDomain: "catch-of-the-day-daniel-cortes.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-daniel-cortes.firebaseio.com",
+});
+
+export default base;
